6a2fe7259a07637476972f3607569bac2f7bcc4f
Sonar69 <156520247+Sonar69@users.noreply.github.com>
2024-12-04 13:04:47 +0100

    delete back, mise a jour dans front

--- Backend/README.md
+++
@@ -1,3 +0,0 @@
-# Node backend
-
-lancer le back : dans le dossier Backend node index.js
--- Frontend/src/Components/WeatherApp.jsx
+++ Frontend/src/Components/WeatherApp.jsx
@@ -15,7 +15,7 @@ const WeatherApp = () => {
     // Fetch cities based on search
     const getCity = async () => {
         try {
-            const response = await axios.get(`http://localhost:3000/weather/${citySearch}`);
+            const response = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${citySearch}&count=100&language=fr&format=json`);
             setFetchCity(response.data.results ? response.data.results : []);
         } catch (error) {
             console.error('Erreur lors de la récupération des données météorologiques', error);
@@ -41,7 +41,7 @@ const WeatherApp = () => {
         if (!latitude || !longitude) return;
 
         try {
-            const response = await axios.get(`http://localhost:3000/weather/${latitude}/${longitude}`);
+            const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&timezone=Europe%2FBerlin`);
             setWeather(response.data);
         } catch (error) {
             console.error('Erreur lors de la récupération des données météorologiques', error);
